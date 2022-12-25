80aa85ba7196e3b6c66c7836b0cfea693b6d581c
Heba Abd Elhamed <90287916+HebaAbdElhamed@users.noreply.github.com>
2022-12-25 02:19:12 +0200

    upload

--- show-Image/.history/routes/web_20221219093505.php
+++ show-Image/.history/routes/web_20221219093505.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\uploadImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +12,7 @@ use App\Http\Controllers\uploadImageController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('photo.welcome');
-});
 
-Route::post('store',[uploadImageController::class ,'store' ])->name('photo.save');
+Route::get('/', function () {
+    return view('welcome');
+});
--- show-Image/.history/routes/web_20221225021701.php
+++ show-Image/.history/routes/web_20221225021701.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\uploadImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +12,7 @@ use App\Http\Controllers\uploadImageController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
+ Route::get('/', function () {
     return view('photo.welcome');
 });
 
